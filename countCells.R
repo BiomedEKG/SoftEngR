2f605fa538a5184588eab3e233b5199fbc88a2e9
MagdalenaSynowiec <magdaa.synowiec@gmail.com>
2015-01-23 16:47:33 +0100

    Liczenie komórek w countCells.R

---
+++ countCells.R
@@ -0,0 +1,14 @@
+countCells <- function(cellArray, stats, t){
+  #c("Healthy", "Ill", "Immune", "Dead")
+  #Zliczanie komorek
+  ill <- sum(cellArray == 1)
+  healthy <- sum(cellArray == 0)
+  cured <- sum(cellArray == 2)
+  dead <- sum(cellArray == 3)
+  #wpisywanie do macierzy
+  stats[t, "Healthy"] <- healthy+cured
+  stats[t, "Ill"] <- ill
+  stats[t, "Immune"] <- cured
+  stats[t, "Dead"] <- dead
+  stats #zwracanie macierzy
+}
