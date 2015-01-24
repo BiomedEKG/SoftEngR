2f8a7d16db863fb5ed2ab67f783e3b159aa47f9e
Patrycja Donath <patidonath@gmail>
2015-01-24 16:46:03 +0100

    UI/UX

---
+++ server.R
@@ -0,0 +1,103 @@
+library('shiny')
+library('plotrix')
+source("initGrid.R")
+source("spreadDisease.R")
+source("countCells.R")
+source("neumann.R")
+source("moore.R")
+
+
+shinyServer(function(input, output) {
+  
+  output$siatka <- renderPlot({
+    
+      postep = input$postep
+      alpha = input$zarazliwosc
+      beta = input$smiertelnosc
+      gamma = input$uzdrowienia
+      neighborhood = input$sasiedztwo
+      N = input$populacja #rozmiar siatki / populacji
+      r = input$promien
+      inicj = input$inicjalizacja
+      ogniska <- input$ogniska
+      #Init 
+      cellArray <- initGrid(N, inicj , ogniska)
+      time = 10
+      
+      # Create data frame for results
+      stats <- matrix(0,time,4)
+      stats <- as.data.frame(stats)
+      col.labels <- c("Healthy", "Infected", "Immune", "Dead")
+      colnames(stats) <- col.labels
+      
+      #for visualization
+      x<- seq(1,N,1)
+      y<- seq(1,N,1)
+      testcol<-color.gradient(c(0,1),0,c(1,0),nslices=4)
+    
+      
+      for (t in 1:postep){
+      #cat(paste("Kolejna chwila czasowa ", t, "\n"))#for debugging
+      #Write to stats how many there are healthy cells etc.
+
+      #spread disease over cell array
+      cellArray <- spreadDisease(cellArray, neighborhood, r, alpha, beta, gamma)
+      #update display
+      image(x, y, cellArray, ylim = rev(range(y)), yaxs = "i", xaxs = "i")
+      par(mar=c(7,4,4,6)) #margin
+      color2D.matplot(cellArray,c(0,1),0,c(1,0), main="Rozwój choroby")
+      color.legend(1.05*N,0.2*N,1.07*N,0.55*N,col.labels,testcol,align="rb",gradient="y")
+      }
+    
+  })
+  
+  output$wykres <- renderPlot({
+    
+    time = input$postep
+    czas <-  c(1:time)
+    alpha = input$zarazliwosc
+    beta = input$smiertelnosc
+    gamma = input$uzdrowienia
+    neighborhood = input$sasiedztwo
+    N = input$populacja #rozmiar siatki / populacji
+    r = input$promien
+    inicj = input$inicjalizacja
+    ogniska <- input$ogniska
+    
+    cellArray <- initGrid(N, inicj , ogniska)
+    
+    stats <- matrix(0,time,4)
+    stats <- as.data.frame(stats)
+    col.labels <- c("Healthy", "Ill", "Immune", "Dead")
+    colnames(stats) <- col.labels
+    #for visualization
+    x<- seq(1,N,1)
+    y<- seq(1,N,1)
+    testcol<-color.gradient(c(0,1),0,c(1,0),nslices=4)
+    #Main loop -> spread disease over time
+    for (t in 1:time){
+      stats<- countCells(cellArray, stats, t)
+      if (stats[t, "Dead"] == N^2){
+        break
+      }
+      #spread disease over cell array
+      cellArray <- spreadDisease(cellArray, neighborhood, r, alpha, beta, gamma)
+    }
+    plot.new()
+    plot.window(xlim=c(0,time), ylim = c(0,N^2))
+    axis(1)
+    axis(2)
+    title(main="Numbers over the days")
+    title(xlab="Days")
+    title(ylab="Number of people")
+    box()
+    lines(czas, stats[,"Immune"], col="pink")
+    lines(czas, stats[,"Healthy"], col="blue")
+    lines(czas, stats[,"Ill"], col="purple")
+    lines(czas, stats[,"Dead"], col="red")
+    legend("topright",col=c("blue", "purple", "pink", "red"), lwd=2,
+           legend=c("Healthy", "Ill", "Immune", "Dead"), cex=0.85, xjust=1, seg.len=0.5)
+    
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,40 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Symulacja epidemii eboli"),
+  
+  # Sidebar with a slider input
+  sidebarLayout(
+    sidebarPanel("Parametry",
+      sliderInput("populacja", "Wielkość populacji:",
+                  min = 0, max = 100, value = 25),
+      radioButtons("inicjalizacja", label = "Inicjalizacja siatki",
+                   choices = list("przypadkowo" = 'rand', "centrum siatki" = 'center'),selected = 'center'),
+      sliderInput("ogniska", "Ilość ognisk:",
+                   min = 0, max = 500, value = 100),
+      radioButtons("sasiedztwo", label = "Sąsiedztwo",
+                  choices = list("von Neumana" = 1, "Moora" = 2),selected = 1),
+      sliderInput("postep", "Postęp epidemii w czasie:", min =  0, max = 100, value = 10, step= 10,
+                  format="## cykli", animate=animationOptions(interval=5000, loop=F)),
+      sliderInput("zarazliwosc", "Współczynnik zaraźliwości:",
+                  min = 0, max = 1, value = 0.01, step= 0.01),
+      sliderInput("smiertelnosc", "Współczynnik śmiertelności:",
+                  min = 0, max = 1, value = 0.03, step= 0.01),
+      sliderInput("uzdrowienia", "Współczynnik uzdrownień:",
+                  min = 0, max = 0.1, value = 0.02, step= 0.001),
+      numericInput("promien", "Promień sąsiedztwa:", min=1, max=4, value=1, step=1)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      h3(textOutput("caption")),
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Wykres", plotOutput("wykres")),  
+                  tabPanel("Siatka", plotOutput("siatka"))
+      )
+    )
+  )
+))
