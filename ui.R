68820a0e325869a4515be5b3c03c127d60015c22
Patrycja Donath <patidonath@gmail>
2015-01-24 22:01:44 +0100

    poprawa sąsiedztwa

--- ui.R
+++ ui.R
@@ -9,23 +9,23 @@ shinyUI(fluidPage(
   # Sidebar with a slider input
   sidebarLayout(
     sidebarPanel("Parametry",
-      sliderInput("populacja", "Wielkość populacji:",
-                  min = 0, max = 100, value = 25),
+      sliderInput("populacja", "Wielkosc populacji:",
+                  min = 0, max = 1000, value = 400),
       radioButtons("inicjalizacja", label = "Inicjalizacja siatki",
                    choices = list("przypadkowo" = 'rand', "centrum siatki" = 'center'),selected = 'center'),
-      sliderInput("ogniska", "Ilość ognisk:",
-                   min = 0, max = 500, value = 100),
-      radioButtons("sasiedztwo", label = "Sąsiedztwo",
-                  choices = list("von Neumana" = 1, "Moora" = 2),selected = 1),
-      sliderInput("postep", "Postęp epidemii w czasie:", min =  0, max = 100, value = 10, step= 10,
+      sliderInput("ogniska", "Ilosc ognisk:",
+                   min = 0, max = 500, value = 50),
+      radioButtons("sasiedztwo", label = "Sasiedztwo",
+                  choices = list("von Neumana" = 'Neumann', "Moora" = 'Moore'),selected = 'Moore'),
+      sliderInput("postep", "Postep epidemii w czasie:", min =  0, max = 100, value = 10, step= 10,
                   format="## cykli", animate=animationOptions(interval=5000, loop=F)),
-      sliderInput("zarazliwosc", "Współczynnik zaraźliwości:",
+      sliderInput("zarazliwosc", "Wspolczynnik zarazliwosci:",
                   min = 0, max = 1, value = 0.01, step= 0.01),
-      sliderInput("smiertelnosc", "Współczynnik śmiertelności:",
+      sliderInput("smiertelnosc", "Wspolczynnik smiertelnosci:",
                   min = 0, max = 1, value = 0.03, step= 0.01),
-      sliderInput("uzdrowienia", "Współczynnik uzdrownień:",
+      sliderInput("uzdrowienia", "Wspolczynnik uzdrownien:",
                   min = 0, max = 0.1, value = 0.02, step= 0.001),
-      numericInput("promien", "Promień sąsiedztwa:", min=1, max=4, value=1, step=1)
+      numericInput("promien", "Promien sasiedztwa:", min=1, max=4, value=1, step=1)
     ),
     
     # Show a plot of the generated distribution
